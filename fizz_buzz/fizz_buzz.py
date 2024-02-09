db1a7c509f5834f72deb7575614ec79156cd7514
Martin Pereira <tin900@hotmail.com>
2024-02-09 19:58:07 -0300

    FizzBuzz fixed

--- fizz_buzz/fizz_buzz.py
+++ fizz_buzz/fizz_buzz.py
@@ -15,3 +15,19 @@ for num in range(5,101,5):
 
     
 print(num_div5, "\n")
+
+
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+n = int(input().strip())
+fizzBuzz(n)
