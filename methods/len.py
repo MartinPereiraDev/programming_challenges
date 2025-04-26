ff4085308e014b25aea5f46767bda24c828eb414
Martin Pereira <tin900@hotmail.com>
2025-04-26 20:16:22 -0300

    function in len

--- methods/len.py
+++ methods/len.py
@@ -9,3 +9,11 @@ the number of elements in the list.
 text = "Python emphasizes code readability and uses indentation (whitespace) to define code blocks"
 text_len = len(text)
 print("number of elements in text: ", text_len)
+
+def pass_len(text):
+    text_len = len(text)
+    return text_len
+
+text = "Python emphasizes code readability and uses indentation (whitespace) to define code blocks"
+text_len = pass_len(text)
+print("number of elements in text with pass_len function: ", text_len)
